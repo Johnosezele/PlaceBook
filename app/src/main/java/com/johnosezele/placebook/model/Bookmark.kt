85e20b600102f58e9b236d28dccd633556a63211
John Osezele <johnosezele@gmail.com>
2023-01-05 23:58:56 +0100

    Added Architecture components to build.gradle
    Setup Entity

---
+++ app/src/main/java/com/johnosezele/placebook/model/Bookmark.kt
@@ -0,0 +1,16 @@
+package com.johnosezele.placebook.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Bookmark(
+    @PrimaryKey(autoGenerate = true) var id: Long? = null,
+    var placeId: String? = null,
+    var name: String = "",
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0,
+    var phone: String = ""
+)
+
+
