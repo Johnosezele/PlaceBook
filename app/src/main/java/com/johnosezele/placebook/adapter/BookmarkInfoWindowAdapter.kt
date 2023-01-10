47e9c5b7007b1c98e9d12e228c7d0072a13bf487
John Osezele <johnosezele@gmail.com>
2023-01-10 20:50:18 +0100

    *solve app crash upon clicking on blue marker. Fixed  imageView.setImageBitmap() mtd, since earlier code assumes that marker.tag contains an object of type MapsActivity.PlaceInfo. However, that's not always the case because a marker can now represent two types of places: one is a temporary place that isn't bookmarked yet, and the other is a place that has an existing bookmark.

--- app/src/main/java/com/johnosezele/placebook/adapter/BookmarkInfoWindowAdapter.kt
+++ app/src/main/java/com/johnosezele/placebook/adapter/BookmarkInfoWindowAdapter.kt
@@ -6,6 +6,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.johnosezele.placebook.databinding.ContentBookmarkInfoBinding
 import com.johnosezele.placebook.ui.MapsActivity
+import com.johnosezele.placebook.viewmodel.MapsViewModel
 
 class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter {
     private val binding = ContentBookmarkInfoBinding.inflate(context.layoutInflater)
@@ -14,7 +15,20 @@ class BookmarkInfoWindowAdapter(context: Activity) : GoogleMap.InfoWindowAdapter
         binding.title.text = marker.title ?: ""
         binding.phone.text = marker.snippet ?: ""
         val imageView = binding.photo
-        imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+        //imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+        //this code is replaced bc: The problem is that this code assumes that marker.tag contains an object of type
+        //MapsActivity.PlaceInfo . However, that's not always the case because a marker can
+        //now represent two types of places: one is a temporary place that isn't bookmarked
+        //yet, and the other is a place that has an existing bookmark. Updated code below
+        when (marker.tag) {
+            is MapsActivity.PlaceInfo -> {
+                imageView.setImageBitmap((marker.tag as MapsActivity.PlaceInfo).image)
+            }
+            is MapsViewModel.BookmarkMarkerView -> {
+                val bookMarkView = marker.tag as MapsViewModel.BookmarkMarkerView
+                //set ImageView bitmap here
+            }
+        }
         return binding.root
     }
 
