9b349718a69c3aad49dcadad0c9ab46a4f2ef583
John Osezele <johnosezele@gmail.com>
2023-01-15 22:53:57 +0100

    *Created details activity

---
+++ app/src/main/java/com/johnosezele/placebook/ui/BookmarkDetailsActivity.kt
@@ -0,0 +1,10 @@
+package com.johnosezele.placebook.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BookmarkDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
