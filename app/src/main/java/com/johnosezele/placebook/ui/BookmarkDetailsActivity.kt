2061cc29a02f2eb81a7b25eedbec2ea52cc4b973
John Osezele <johnosezele@gmail.com>
2023-01-15 22:59:41 +0100

    *Added Function ppties

--- app/src/main/java/com/johnosezele/placebook/ui/BookmarkDetailsActivity.kt
+++ app/src/main/java/com/johnosezele/placebook/ui/BookmarkDetailsActivity.kt
@@ -2,9 +2,23 @@ package com.johnosezele.placebook.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.johnosezele.placebook.R
+import com.johnosezele.placebook.databinding.ActivityBookmarkDetailsBinding
 
 class BookmarkDetailsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var databinding: ActivityBookmarkDetailsBinding
+
+        override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
+        databinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_bookmark_details
+        )
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(databinding.toolbar)
     }
 }
