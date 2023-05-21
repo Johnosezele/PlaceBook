c824ecae1949b23e835e7eb3c724fda14006ff20
John Osezele <johnosezele@gmail.com>
2023-05-21 04:27:11 +0100

    Feature added:
    *added code to capture image when the Camera option is selected from the Photo Option dialog

--- app/src/main/java/com/johnosezele/placebook/ui/BookmarkDetailsActivity.kt
+++ app/src/main/java/com/johnosezele/placebook/ui/BookmarkDetailsActivity.kt
@@ -1,14 +1,18 @@
 package com.johnosezele.placebook.ui
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import com.johnosezele.placebook.R
 import com.johnosezele.placebook.databinding.ActivityBookmarkDetailsBinding
+import com.johnosezele.placebook.util.ImageUtils
 import com.johnosezele.placebook.viewmodel.BookmarkDetailsViewModel
 import java.io.File
 
@@ -101,8 +105,43 @@ class BookmarkDetailsActivity : AppCompatActivity(),
         }
 
     override fun onCaptureClick() {
-        Toast.makeText(this, "Camera Capture",
-        Toast.LENGTH_SHORT).show()
+        //Any previously assigned photoFile is cleared.
+        photoFile = null
+        //call createUniqueImageFile() to create a uniquely named image File and
+        //assign it to photoFile
+        //If an exception is thrown, the method returns without doing anything.
+        try {
+            photoFile = ImageUtils.createUniqueImageFile(this)
+        } catch (ex: java.io.IOException){
+            return
+        }
+        //use the ?.let to make sure photoFile is not null before continuing with
+        //the rest of the method
+        photoFile?.let {photoFile ->
+            //called to get a Uri for the temporary photo file
+            val photoUri = FileProvider.getUriForFile(this,
+            "com.johnosezele.placebook.fileprovider", photoFile)
+
+            //A new Intent is created with the ACTION_IMAGE_CAPTURE action. This Intent is
+            //used to display the camera viewfinder and allow the user to snap a new photo.
+            val captureIntent =
+                Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+
+            //The photoUri is added as an extra on the Intent, so the Intent knows where to
+            //save the full-size image captured by the user.
+            captureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, photoUri)
+
+            //Temporary write permissions on the photoUri are given to the Intent
+            val intentActivities = packageManager.queryIntentActivities(
+                captureIntent, PackageManager.MATCH_DEFAULT_ONLY
+            )
+            intentActivities.map { it.activityInfo.packageName }
+                .forEach { grantUriPermission(it, photoUri,
+                Intent.FLAG_GRANT_WRITE_URI_PERMISSION) }
+
+            //The Intent is invoked, and the request code REQUEST_CAPTURE_IMAGE is passed in.
+            startActivityForResult(captureIntent, REQUEST_CAPTURE_IMAGE)
+        }
     }
 
     override fun onPickClick() {
@@ -116,9 +155,9 @@ class BookmarkDetailsActivity : AppCompatActivity(),
         newFragment?.show(supportFragmentManager, "photoOptionDialog")
     }
 
-    //define a request code to identify the request when image capture activity returns the image
+   //defines the request code to use when processing the camera capture Intent
     companion object{
-
+        private const val REQUEST_CAPTURE_IMAGE = 1
     }
 
 }
